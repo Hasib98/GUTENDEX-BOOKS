aafc66364549a6520f5dbdfea752c4306e37fd4e
Hasib98 <phoenixoffire1998@gmail.com>
2024-10-20 23:47:45 +0600

    project add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import Title from "./Title";
 
 export default function Navbar() {
   return (
-    <div className=" bg-red-300 flex items-center justify-between pl-10 pr-10 pt-5 pb-5">
+    <div className=" bg-gray-300 flex items-center justify-between ">
       <Title />
       <Search />
       <div>Count: 12</div>
