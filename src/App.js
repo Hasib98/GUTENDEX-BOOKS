91ba4103fede6dba3c15c1f0eda6d9c20d09d8db
Hasib98 <phoenixoffire1998@gmail.com>
2024-10-23 08:05:12 +0600

    LocaStorage Wishlist Feature Added

--- src/App.js
+++ src/App.js
@@ -157,13 +157,13 @@ function RightArrow({ data, onSetPageNumber }) {
 function Loader() {
   return (
     <div className="flex flex-col  gap-10 items-center justify-center min-h-screen">
-      <div className="text-3xl">
-        Please wait, Sometimes It takes time load the Books (loading..⏳)
-      </div>
       <div className="flex space-x-2">
         <div className="w-4 h-4 bg-amber-500 rounded-full animate-bounce delay-100"></div>
         <div className="w-4 h-4 bg-amber-500 rounded-full animate-bounce delay-500"></div>
         <div className="w-4 h-4 bg-amber-500 rounded-full animate-bounce delay-1000"></div>
+      </div>
+      <div className="text-3xl">
+        Please wait, Sometimes It takes time load the Books (loading..⏳)
       </div>
     </div>
   );
